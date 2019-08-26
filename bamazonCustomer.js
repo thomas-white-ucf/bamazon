a1cc4a75459e38efeb7ff3132ab6cf5ce1b5c22f
Thomas White <thomas.white.ucf@gmail.com>
2019-08-26 15:20:07 -0400

    Items get displayed then Inquirer prompts for user input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,28 +19,48 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
 	if (err) throw err;
-	// run the start function after the connection is made to prompt the user
-	start();
+	// run the display function after the connection is made to prompt the user with the available inventory
+	console.log('BAMAZON - Products For Sale - $');
+	displayProducts();
 });
 
-// function which prompts the user for what action they should take
-function start() {
+function displayProducts() {
+	connection.query('SELECT * FROM products', function(err, res) {
+		if (err) throw err;
+		// console.log(res);
+		console.log('ID   -   Product   -   Department   -  $ Price $')
+		for(let i=0; i<res.length; i++){
+			let item = res[i]
+			console.log(item.id + "  ", item.product_name + "      ", item.department + "    ", item.price)
+		}
+	});
+	questionPrompt();
+}
+
+// function which prompts the user for what Product they would like to purchase
+function questionPrompt() {
 	inquirer
 		.prompt({
-			name: 'postOrBid',
-			type: 'list',
-			message: 'Would you like to [POST] an auction or [BID] on an auction?',
-			choices: [ 'POST', 'BID', 'EXIT' ]
+			type: 'input',
+			name: 'customerProduct',
+			message: 'What is the ID of the Product you would like to purchase?'
+		},{
+			type: 'input',
+			name: 'purchaseCount',
+			message: 'How many units of that product would you like to purchase?'
 		})
 		.then(function(answer) {
-            console.log(answer)
-			// based on their answer, either call the bid or the post functions
-			// if (answer.postOrBid === 'POST') {
-			// 	postAuction();
-			// } else if (answer.postOrBid === 'BID') {
-			// 	bidAuction();
-			// } else {
-			// 	connection.end();
-			// }
+			console.log(answer);
+// based on their answer, either call the bid or the post functions
+// if (answer.customerProduct === 'POST') {
+// 	postAuction();
+// } else if (answer.customerProduct === 'BID') {
+// 	bidAuction();
+// } else {
+// 	connection.end();
+// }
+
 		});
 }
+
+//   connection.end();
--- working.sql
+++ working.sql
@@ -13,15 +13,16 @@ USE bamazon_db;
 -- );
 
 -- INSERT INTO products (product_name, department, price, stock_quantity)
--- VALUES ("iphone Charger", "Bamazon Basics", 11, 12345),
---        ("USB Charger", "Bamazon Basics", 9, 1200),
---        ("Alexa-Siri Combo", "Bamazon Basics", 90000, 2),
---        ("Macbook", "Computers", 11, 55),
---        ("Dell XPS", "Computers", 11, 65),
---        ("Surface Pro 3", "Computers", 11, 75),
---        ("Tesla Model X", "Electric Cars", 120000, 200),
---        ("Nissan LEAF", "Electric Cars", 25999, 30),
---        ("Audi e-tron", "Electric Cars", 31099, 40),
---        ("Fisker Karma", "Electric Cars", 120001, 5);
+-- VALUES ("iphone Charger",                "Bamazon Basics",               11,                     12345),
+--        ("USB Charger",               "Bamazon Basics",               9,                  1200),
+--        ("Alexa-Siri Combo",              "Bamazon Basics",           90000,                  2),
+--        ("Macbook",               "Computers",                11,                     55),
+--        ("Dell XPS",              "Computers",                11,                     65),
+--        ("Surface Pro 3",                 "Computers",                11,                     75),
+--        ("Tesla Model X",                 "Electric Cars",                120000,                     200),
+--        ("Nissan LEAF",               "Electric Cars",                25999,                  30),
+--        ("Audi e-tron",               "Electric Cars",                31099,                  40),
+--        ("Fisker Karma",              "Electric Cars",                120001,                     5);
 
-SELECT * FROM products;
+-- SELECT * FROM products;
+-- SELECT product_name, department, price FROM products
