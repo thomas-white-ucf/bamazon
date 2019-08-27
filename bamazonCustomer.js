58a0931af10870723a240190211b51b8b10043da
Thomas White <thomas.white.ucf@gmail.com>
2019-08-26 22:30:31 -0400

    check for quantity in stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,9 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+// let moment = require('moment');
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
 	host: 'localhost',
 
 	// Your port; if not 3306
@@ -20,47 +21,70 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
 	if (err) throw err;
 	// run the display function after the connection is made to prompt the user with the available inventory
-	console.log('BAMAZON - Products For Sale - $');
-	displayProducts();
+	displayTitle();
 });
+
+// Prompt title - Bamazon products for sale - $
+function displayTitle() {
+	inquirer
+		.prompt({
+			name: 'q1',
+			type: 'input',
+			message: '\nBAMAZON - Products For Sale - $\n\n Press ENTER to display products!'
+		})
+		.then(function(answers) {
+			displayProducts();
+		});
+}
 
 function displayProducts() {
 	connection.query('SELECT * FROM products', function(err, res) {
 		if (err) throw err;
 		// console.log(res);
-		console.log('ID   -   Product   -   Department   -  $ Price $')
-		for(let i=0; i<res.length; i++){
-			let item = res[i]
-			console.log(item.id + "  ", item.product_name + "      ", item.department + "    ", item.price)
+		// console.table(res);
+
+		console.log('ID   -   Product   -   Department   -  $ Price $');
+		for (let i = 0; i < res.length; i++) {
+			let item = res[i];
+			console.log(item.id + ' - ', item.product_name + '   -   ', item.department + '  -  $', item.price);
 		}
+		questionPrompt();
 	});
-	questionPrompt();
 }
+
+// Question array for following inquirer
+let questions = [
+	{
+		name: 'customerProduct',
+		type: 'input',
+		message: '\nWhat is the ID of the Product you would like to purchase?'
+	},
+	{
+		name: 'customerCount',
+		type: 'input',
+		message: '\nHow many units of that product would you like to purchase?'
+	}
+];
 
 // function which prompts the user for what Product they would like to purchase
 function questionPrompt() {
-	inquirer
-		.prompt({
-			type: 'input',
-			name: 'customerProduct',
-			message: 'What is the ID of the Product you would like to purchase?'
-		},{
-			type: 'input',
-			name: 'purchaseCount',
-			message: 'How many units of that product would you like to purchase?'
-		})
-		.then(function(answer) {
-			console.log(answer);
-// based on their answer, either call the bid or the post functions
-// if (answer.customerProduct === 'POST') {
-// 	postAuction();
-// } else if (answer.customerProduct === 'BID') {
-// 	bidAuction();
-// } else {
-// 	connection.end();
-// }
+	inquirer.prompt(questions).then(function(answers) {
+		// console.log(`id=${answers.customerProduct}  and  ordernumber=${answers.customerCount}`);
 
+		// based on customer input for product ID, check the quantity of that product
+		connection.query(`SELECT * FROM products WHERE id=${answers.customerProduct}`, function(err, res) {
+			if (err) throw err;
+
+			// Check if quantity in stock is less than demanded by customer
+			if (answers.customerCount > res[0].stock_quantity) {
+				console.log(`Insufficient quantity!`);
+			} else {
+				console.log(res[0].stock_quantity, 'units available');
+			}
 		});
-}
 
-//   connection.end();
+		// else {
+		// 	connection.end();
+		// }
+	});
+}
